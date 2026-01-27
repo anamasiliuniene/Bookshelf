954ea795097870bc54210d88bf3c2e8282e0c44b
anamasiliuniene <ana.masiliuniene@gmail.com>
2026-01-27 15:31:38 +0200

    aprasytas registruotas Book modelis

--- mysite/bookshelf/models.py
+++ mysite/bookshelf/models.py
@@ -15,3 +15,15 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
+class Book(models.Model):
+    title = models.CharField()
+    author = models.ForeignKey(to="Author",
+                               on_delete=models.SET_NULL,
+                               null=True, blank=True)
+    summary = models.TextField()
+    isbn = models.CharField(max_length=13)
+    genre = models.ManyToManyField(to="Genre")
+
+    def __str__(self):
+        return self.title
