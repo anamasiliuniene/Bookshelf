d7e033bf9a724a018b43c0907bb943296020fd3c
anamasiliuniene <ana.masiliuniene@gmail.com>
2026-01-27 15:13:38 +0200

    aprasytas registruotas Genre modelis

--- mysite/bookshelf/admin.py
+++ mysite/bookshelf/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Author
+from .models import Author, Genre
 
 
 # Register your models here.
 admin.site.register(Author)
+admin.site.register(Genre)
--- mysite/bookshelf/models.py
+++ mysite/bookshelf/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Author(models.Model):
     first_name = models.CharField()
@@ -7,3 +8,10 @@ class Author(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField()
+
+    def __str__(self):
+        return self.name
