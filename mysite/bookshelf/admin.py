36f47fd7e1e5eddbbd49ef5d4593cd01e01693bd
anamasiliuniene <ana.masiliuniene@gmail.com>
2026-01-28 12:11:14 +0200

    Pridėtas BookInstanceInLine knygų kortelėje

--- mysite/bookshelf/admin.py
+++ mysite/bookshelf/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
 
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ["title", "author", "display_genre"]
-
+    inlines = [BookInstanceInline]
 
 class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ["book", "status", "due_back", "uuid"]
