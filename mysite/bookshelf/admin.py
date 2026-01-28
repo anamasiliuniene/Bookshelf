e0a47e5d79fa39747534825db71a4432a29bb4ff
anamasiliuniene <ana.masiliuniene@gmail.com>
2026-01-28 12:06:23 +0200

    Pridėti BookInstance laukai

--- mysite/bookshelf/admin.py
+++ mysite/bookshelf/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ["title", "author", "display_genre"]
 
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ["book", "status", "due_back", "uuid"]
+    list_filter = ["book", "status", "due_back"]
+
+    fieldsets = (
+    ('General', {
+        'fields': ('book', 'uuid'),
+    }),
+    ('Availability', {
+        'fields': ('status', 'due_back'),
+    }),
+)
 
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(Book, BookAdmin)
-admin.site.register(BookInstance)
+admin.site.register(BookInstance, BookInstanceAdmin)
