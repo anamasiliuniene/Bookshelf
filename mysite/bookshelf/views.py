a6c6957bc55ff9cbab8b5f010961ac9fa17989e1
anamasiliuniene <ana.masiliuniene@gmail.com>
2026-01-29 11:13:42 +0200

    4 paskaita: index.html atvaizduojama bibliotekos statistika per jinge2

--- mysite/bookshelf/views.py
+++ mysite/bookshelf/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance
 
 # Create your views here.
 def index(request):
-    return render(request, 'bookshelf/index.html')
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'bookshelf/index.html', context=context)
