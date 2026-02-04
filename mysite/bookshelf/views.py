7f0ffb59499e3e514b1f4bb674d58a7c2ee8d278
anamasiliuniene <ana.masiliuniene@gmail.com>
2026-02-04 12:29:40 +0200

    ideta autoriu ir knygu paieska

--- mysite/bookshelf/views.py
+++ mysite/bookshelf/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Book, Author, BookInstance
 from django.core.paginator import Paginator
-
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -48,3 +48,17 @@ class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'bookshelf/book.html'
     context_object_name = 'book'
+
+def search(request):
+    query = request.GET.get('query')
+    context = {
+        "query": query,
+        "books": Book.objects.filter(Q(title__icontains=query) |
+                                     Q(author__first_name__icontains=query) |
+                                     Q(author__last_name__icontains=query) |
+                                     Q(summary__icontains=query)),
+
+        "authors": Author.objects.filter (Q(first_name__icontains=query) |
+                                     Q(last_name__icontains=query))
+    }
+    return render(request, 'bookshelf/search.html', context=context)
